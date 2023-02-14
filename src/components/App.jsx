d1e6c23113d82a2a28bef2b65ffc3504dd85d040
Alexey Gorchakov <amvrogor@gmail.com>
2023-02-15 01:41:48 +0200

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,13 @@
 import { Profile } from "./Profile";
+import { Statistics } from "./Statistics";
 import user from "../user.json"
+import data from "../data.json"
 
 export const App = () => {
   return (
     <div>
       <Profile user={user} />
+      <Statistics stats={data} />
     </div>
   );
 };
-
-
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
---
+++ src/components/Stats.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+export const Stats = ({item: {label, percentage}}) => {
+    return (       
+    <div>
+      <span className="label">{label}</span>
+      <span className="percentage">{percentage}</span>
+    </div> 
+    )
+}
+
+Stats.propTypes = {
+    item: PropTypes.shape({
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number.isRequired,
+    })
+}
