0675e07106e949a2b9b84eabbea84d8c5a4ca17b
Alexey Gorchakov <amvrogor@gmail.com>
2023-02-14 13:59:55 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,7 @@ import user from "../user.json"
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <div>
       <Profile user={user} />
     </div>
   );
